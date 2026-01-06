645a6ef2201bc2f7b8aed8210c68f7c2fbbb1c2f
changjunYun <163440796+changjunYun@users.noreply.github.com>
2026-01-06 16:16:50 +0900

    Updates

---
+++ _next/static/2hK5ZvBLG0RNL9Pva78eE/_buildManifest.js
@@ -0,0 +1,19 @@
+self.__BUILD_MANIFEST = {
+  "/_error": [
+    "./static/chunks/24376143c0ee8a7b.js"
+  ],
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/sample//_next/:path+",
+        "destination": "/sample/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
---
+++ _next/static/chunks/290ddd8a7cc3c142.js
@@ -0,0 +1,5 @@
+__turbopack_load_page_chunks__("/_app", [
+  "static/chunks/e60ef129113f6e24.js",
+  "static/chunks/c6866888ea32210d.js",
+  "static/chunks/turbopack-56182c8058d9db93.js"
+])
