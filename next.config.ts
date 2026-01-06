76e102463a87fd02640ea1b025e03d4c7a8bcfd0
changjunYun <163440796+changjunYun@users.noreply.github.com>
2026-01-06 15:27:46 +0900

    이미지 깨짐 현상 해결

--- next.config.ts
+++ next.config.ts
@@ -3,11 +3,10 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: 'export',
   images: {
-    formats: ["image/avif", "image/webp"],
-    qualities: [75, 85],
+    unoptimized: true,
   },
   basePath: '/sample', // <--- 내 깃허브 프로젝트 이름(주소 맨 뒤)을 슬래시와 함께 넣으세요.
-  assetPrefix: '/sample', // <--- 똑같이 한 번 더 적어주세요.
+  assetPrefix: '/sample/', // <--- 똑같이 한 번 더 적어주세요.
 };
 
 export default nextConfig;
